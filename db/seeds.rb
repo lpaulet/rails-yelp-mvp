8656aa738c4712488aff690579cf0ee76857072a
Louis Paulet <louis.paulet.14@neoma-bs.com>
2019-05-22 15:21:50 +0200

    Rails Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::Number.number(10), category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save
+  puts restaurant.errors.full_messages
+end
+
+puts 'Finished!'
